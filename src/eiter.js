e181b4d899720030540c1320888d61e5e1e1146f
Shane Xu <xusheng0711@gmail.com>
2016-09-06 16:10:18 +0800

    add rights lefts partitionEithers isLeft isRight

--- src/eiter.js
+++ src/eiter.js
@@ -1,5 +1,5 @@
-import { identity, curry } from 'lodash/fp'
-import { _instance, _case } from './utils.js'
+import { identity, curry, filter } from 'lodash/fp'
+import { _instance, _case, _extend } from './utils.js'
 import Functor, { fmap } from './functor.js'
 import Applicative from './applicative.js'
 import Monad from './monad.js'
@@ -17,6 +17,8 @@ export class Either{
     return either(f, g, this)
   }
 }
+
+_extend(Either.prototype, Functor._methods, Applicative._methods, Monad._methods)
 
 export class Right extends Either {
   constructor(value) {
@@ -38,10 +40,90 @@ export function left(value) {
   return new Left(value)
 }
 
+// | Case analysis for the 'Either' type.
+// If the value is @'Left' a@, apply the first function to @a@;
+// if it is @'Right' b@, apply the second function to @b@.
+//
+// ==== __Examples__
+//
+// We create two values of type @'Either' 'String' 'Int'@, one using the
+// 'Left' constructor and another using the 'Right' constructor. Then
+// we apply \"either\" the 'length' function (if we have a 'String')
+// or the \"times-two\" function (if we have an 'Int'):
+//
+// >>> let s = Left "foo" :: Either String Int
+// >>> let n = Right 3 :: Either String Int
+// >>> either length (*2) s
+// 3
+// >>> either length (*2) n
+// 6
+//
+// either                  :: (a -> c) -> (b -> c) -> Either a b -> c
 export let either = curry((f, g, e) => _case(e).of([
   [Left,   ({value:x}) => f(x)]
   ,[Right, ({value:y}) => g(y)]
 ]))
+
+export let isLeft = e => _case(e).of([
+  [Left,   () => true]
+  ,[Right, () => false]
+])
+
+export let isRight = e => _case(e).of([
+  [Left,   () => false]
+  ,[Right, () => true]
+])
+
+// | Partitions a list of 'Either' into two lists.
+// All the 'Left' elements are extracted, in order, to the first
+// component of the output.  Similarly the 'Right' elements are extracted
+// to the second component of the output.
+//
+// ==== __Examples__
+//
+// Basic usage:
+//
+// >>> let list = [ Left "foo", Right 3, Left "bar", Right 7, Left "baz" ]
+// >>> partitionEithers list
+// (["foo","bar","baz"],[3,7])
+//
+// The pair returned by @'partitionEithers' x@ should be the same
+// pair as @('lefts' x, 'rights' x)@:
+//
+// >>> let list = [ Left "foo", Right 3, Left "bar", Right 7, Left "baz" ]
+// >>> partitionEithers list == (lefts list, rights list)
+// True
+//
+// partitionEithers :: [Either a b] -> ([a],[b])
+export let partitionEithers = es => [lefts(es), rights(es)]
+
+// | Extracts from a list of 'Either' all the 'Left' elements.
+// All the 'Left' elements are extracted in order.
+//
+// ==== __Examples__
+//
+// Basic usage:
+//
+// >>> let list = [ Left "foo", Right 3, Left "bar", Right 7, Left "baz" ]
+// >>> lefts list
+// ["foo","bar","baz"]
+//
+// lefts   :: [Either a b] -> [a]
+export let lefts = filter(isLeft)
+
+// | Extracts from a list of 'Either' all the 'Right' elements.
+// All the 'Right' elements are extracted in order.
+//
+// ==== __Examples__
+//
+// Basic usage:
+//
+// >>> let list = [ Left "foo", Right 3, Left "bar", Right 7, Left "baz" ]
+// >>> rights list
+// [3,7]
+//
+// rights   :: [Either a b] -> [b]
+export let rights = filter(isRight)
 
 _instance(Functor, Either).where({
   fmap: (func, e) => _case(e).of([
