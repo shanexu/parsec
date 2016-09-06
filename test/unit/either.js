65d1f86ed708b6f34a0d90e655742be09a06d342
Shane Xu <xusheng0711@gmail.com>
2016-09-06 16:35:08 +0800

    add unit test

---
+++ test/unit/either.js
@@ -0,0 +1,38 @@
+import { Right, Left, right, left } from '../../src/eiter.js'
+
+describe('either', () => {
+  it('should fmap Left', () => {
+    let m = left('hello')
+    let m1 = m.map(() => 1)
+    expect(m1).to.be.instanceOf(Left)
+    expect(m1.value).to.equal('hello')
+
+    let m2 = (() => 1) ['fmap'] (m)
+    expect(m2).to.be.instanceOf(Left)
+    expect(m2.value).to.equal('hello')
+  })
+
+  it('should fmap Right', () => {
+    let m = right('hello')
+    let m1 = m.map(() => 1)
+    expect(m1).to.be.instanceOf(Right)
+    expect(m1.value).to.equal(1)
+
+    let m2 = (() => 1) ['fmap'] (m)
+    expect(m2).to.be.instanceOf(Right)
+    expect(m2.value).to.equal(1)
+  })
+
+  it('should <*> Left', () => {
+    let m = left(i => i + 1)
+    let m1 = (m) ['<*>'] (right(1))
+    expect(m1).to.be.instanceOf(Left)
+  })
+
+  it('should <*> Right', () => {
+    let m = right(i => i + 1)
+    let m1 = (m) ['<*>'] (right(1))
+    expect(m1).to.be.instanceOf(Right)
+    expect(m1.value).to.equal(2)
+  })
+})
