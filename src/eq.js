ca90b127df3d34978bc46c3e64866ed4620b447a
Shane Xu <xusheng0711@gmail.com>
2016-09-06 22:06:10 +0800

    add eq

---
+++ src/eq.js
@@ -0,0 +1,39 @@
+import { _instance_method, _instance, _extend } from './utils.js'
+import { curry, isEqual } from 'lodash/fp'
+
+let equal = curry((a1, a2) => _instance_method(Eq, '==', a1)(a1, a2))
+let notEqual = curry((a1, a2) => !equal(a1,a2))
+let Eq = {
+  '==': equal
+  ,'/=': notEqual
+
+  ,_methods: {
+    '==': function(other){
+      return Eq['=='](this, other)
+    }
+    ,'/=': function(other) {
+      return Eq['/='](this, other)
+    }
+  }
+
+}
+
+module.exports = Eq
+
+_instance(Eq, Array).where({
+  '==': isEqual
+})
+
+_extend(Array.prototype, Eq._methods)
+
+_instance(Eq, Number).where({
+  '==': (a,b) => a === b
+})
+
+_extend(Number.prototype, Eq._methods)
+
+_instance(Eq, String).where({
+  '==': (a,b) => a === b
+})
+
+_extend(String.prototype, Eq._methods)
