baa0b8901d5ac8a804d9fc2d85df1447c6524144
Shane Xu <xusheng0711@gmail.com>
2016-09-06 22:06:48 +0800

    add ord

---
+++ test/unit/ord.js
@@ -0,0 +1,19 @@
+import { compare } from '../../src/ord.js'
+import { LT, EQ, GT } from '../../src/ordering.js'
+import '../../src/eq.js'
+
+describe('Ord', () => {
+  it('should number compare', () => {
+    expect(compare(1, 2)).to.equal(LT)
+    expect(compare(1, 1)).to.equal(EQ)
+    expect(compare(3, 1)).to.equal(GT)
+  })
+
+  it('should number operations', () => {
+    let a1 = 1, a2 = 3
+    expect((a1) ['<'] (a2)).to.be.true
+    expect((a1) ['>'] (a2)).to.be.false
+    expect((a1) ['=='] (a2)).to.be.false
+    expect((a1) ['=='] (1)).to.be.true
+  })
+})
