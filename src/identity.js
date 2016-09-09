ddce3fe683761a7f04ae1b0642f394be8fe1c933
Shane Xu <xusheng0711@gmail.com>
2016-09-09 16:19:21 +0800

    add identity

---
+++ src/identity.js
@@ -0,0 +1,33 @@
+import { compose } from 'lodash/fp'
+import { _extend, _instance } from './utils.js'
+import Functor from './functor.js'
+import Applicative from './applicative.js'
+import Monad from './monad.js'
+
+
+export class Identity {
+  constructor(i) {
+    this.runIdentity = i
+  }
+}
+
+export let Identity_ = i => new Identity(i)
+
+export let runIdentity = id => id.runIdentity
+
+_instance(Functor, Identity).where({
+  fmap: (f, i) => compose(Identity_, f, runIdentity)(i)
+})
+
+_instance(Applicative, Identity).where({
+  pure: i => Identity_(i),
+  '<*>': (f, i) => compose(Identity_, runIdentity(f), runIdentity)(i)
+})
+
+_instance(Monad, Identity).where({
+  return: i => Identity_(i),
+  '>>=': (m, k) => compose(k, runIdentity)(m)
+})
+
+_extend(Identity.prototype, Functor._methods, Applicative._methods, Monad._methods)
+_extend(Identity, Functor._static_methods, Applicative._static_methods, Monad._static_methods)
