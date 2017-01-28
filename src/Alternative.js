b44f9850f9ad916c37daabf9e0fe91ef5ef834db
Shane Xu <xusheng0711@gmail.com>
2017-01-28 19:17:22 +0800

    format

--- src/Alternative.js
+++ src/Alternative.js
@@ -1,31 +1,39 @@
-import { _instance_method, _instance, _extend } from './utils.js'
-import { curry, concat } from 'lodash/fp'
+import {
+  _instance_method,
+  _instance,
+  _extend
+} from './utils.js'
+import {
+  curry,
+  concat
+} from 'lodash/fp'
 
 let empty = type => _instance_method(Alternative, 'empty', type)
-let f1 = curry((a, b) => _instance_method(Alternative, '<|>', a)(a,b))
+let f1 = curry((a, b) => _instance_method(Alternative, '<|>', a)(a, b))
 let Alternative = {
   // The identity of '<|>'
-  empty
+  empty,
 
   // An associative binary operation
   // f a -> f a -> f a
-  ,'<|>': f1
-  ,_methods: {
+  '<|>': f1,
+  _methods: {
     '<|>': function(other) {
       return Alternative['<|>'](this, other)
     }
-  }
-  ,_static_methods: {
+  },
+  _static_methods: {
     empty: function() {
       return Alternative['empty'](this)
     }
   }
 }
 
-_instance(Alternative, Array).where({
-  empty: [],
-  '<|>': concat
-})
+_instance(Alternative, Array)
+  .where({
+    empty: [],
+    '<|>': concat
+  })
 
 _extend(Array.prototype, Alternative._methods)
 _extend(Array, Alternative._static_methods)
