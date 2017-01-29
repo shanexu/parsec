628f893abc37189ed2538b0182211d88cf586234
Shane Xu <xusheng0711@gmail.com>
2017-01-29 09:56:15 +0800

    rename file

--- src/Data/Maybe.js
+++ src/Data/Maybe.js
@@ -1,22 +1,29 @@
-import { _instance, _extend } from './utils.js'
-import Functor, { fmap } from './Functor.js'
-import Applicative from './Applicative.js'
-import Monad from './Monad.js'
-import Alternative from './Alternative.js'
-import { _case } from './utils.js'
+import {
+  _instance,
+  _extend,
+  _case
+} from 'utils.js'
+import Functor, {
+  fmap
+} from 'Functor.js'
+import Applicative from 'Applicative.js'
+import Monad from 'Monad.js'
+import Alternative from 'Alternative.js'
 
 const payload = {}
 
-export class Maybe{
+export class Maybe {
   constructor(x) {
-    if(x !== payload) {
+    if (x !== payload) {
       throw new TypeError('Maybe is an abstract class')
     }
   }
 }
 
-_extend(Maybe.prototype, Functor._methods, Applicative._methods, Monad._methods, Alternative._methods)
-_extend(Maybe, Functor._static_methods, Applicative._static_methods, Monad._static_methods, Alternative._static_methods)
+_extend(Maybe.prototype, Functor._methods, Applicative._methods, Monad._methods,
+  Alternative._methods)
+_extend(Maybe, Functor._static_methods, Applicative._static_methods, Monad._static_methods,
+  Alternative._static_methods)
 
 export class Just extends Maybe {
   constructor(value) {
@@ -38,33 +45,47 @@ export let Nothing = (() => {
   return new Nothing()
 })()
 
-_instance(Functor, Maybe).where({
-  fmap: (func, m) => _case(m).of([
-    [Nothing, () => Nothing]
-    ,[Just,   ({value}) => just(func(value))]
-  ])
-})
+_instance(Functor, Maybe)
+  .where({
+    fmap: (func, m) => _case(m)
+      .of([
+        [Nothing, () => Nothing],
+        [Just, ({
+          value
+        }) => just(func(value))]
+      ])
+  })
 
-_instance(Applicative, Maybe).where({
-  pure: just,
-  '<*>': (mf, m) => _case(mf).of([
-    [Nothing, () => Nothing]
-    ,[Just,   ({value: f}) => fmap(f, m)]
-  ])
-})
+_instance(Applicative, Maybe)
+  .where({
+    pure: just,
+    '<*>': (mf, m) => _case(mf)
+      .of([
+        [Nothing, () => Nothing],
+        [Just, ({
+          value: f
+        }) => fmap(f, m)]
+      ])
+  })
 
-_instance(Monad, Maybe).where({
-  '>>=': (m, k) => _case(m).of([
-    [Nothing, () => Nothing]
-    ,[Just,   ({value: x}) => k(x)]
-  ])
-  ,'fail': () => Nothing
-})
+_instance(Monad, Maybe)
+  .where({
+    '>>=': (m, k) => _case(m)
+      .of([
+        [Nothing, () => Nothing],
+        [Just, ({
+          value: x
+        }) => k(x)]
+      ]),
+    'fail': () => Nothing
+  })
 
-_instance(Alternative, Maybe).where({
-  empty: Nothing,
-  '<|>': (l, r) => _case(l).of([
-    [Nothing, () => r]
-    ,[Just,   () => l]
-  ])
-})
+_instance(Alternative, Maybe)
+  .where({
+    empty: Nothing,
+    '<|>': (l, r) => _case(l)
+      .of([
+        [Nothing, () => r],
+        [Just, () => l]
+      ])
+  })
