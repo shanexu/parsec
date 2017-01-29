8ac8e48d940d0dcdb2937abf0488b61e5d3772b4
Shane Xu <xusheng0711@gmail.com>
2017-01-29 10:06:29 +0800

    rename files

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -1,4 +1,6 @@
-import { _case } from '../../src/utils.js'
+import {
+  _case
+} from 'utils.js'
 
 describe('utils', () => {
   describe('should case of', () => {
@@ -20,50 +22,62 @@ describe('utils', () => {
 
     it('should instanceof B', () => {
       let b = new B(1)
-      let res = _case(b).of([
-        [B, () => 'B'],
-        [A, () => 'A']
-      ])
-      expect(res).to.equal('B')
+      let res = _case(b)
+        .of([
+          [B, () => 'B'],
+          [A, () => 'A']
+        ])
+      expect(res)
+        .to.equal('B')
     })
 
     it('should instanceof A', () => {
       let b = new B(1)
-      let res = _case(b).of([
-        [A, () => 'A'],
-        [B, () => 'B']
-      ])
-      expect(res).to.equal('A')
+      let res = _case(b)
+        .of([
+          [A, () => 'A'],
+          [B, () => 'B']
+        ])
+      expect(res)
+        .to.equal('A')
     })
 
     it('should equal a B instance', () => {
       let b = new B(1)
-      let res = _case(b).of([
-        [new B(1), () => 'new B(1)'],
-        [A, () => 'A'],
-        [B, () => 'B']
-      ])
-      expect(res).to.equal('new B(1)')
+      let res = _case(b)
+        .of([
+          [new B(1), () => 'new B(1)'],
+          [A, () => 'A'],
+          [B, () => 'B']
+        ])
+      expect(res)
+        .to.equal('new B(1)')
     })
 
     it('should use condition predict', () => {
       let b = new B(1)
-      let res = _case(b).of([
-        [({value}) => value === 1, () => '({value}) => value === 1'],
-        [new B(1), () => 'new B(1)'],
-        [A, () => 'A'],
-        [B, () => 'B']
-      ])
-      expect(res).to.equal('({value}) => value === 1')
+      let res = _case(b)
+        .of([
+          [({
+            value
+          }) => value === 1, () => '({value}) => value === 1'],
+          [new B(1), () => 'new B(1)'],
+          [A, () => 'A'],
+          [B, () => 'B']
+        ])
+      expect(res)
+        .to.equal('({value}) => value === 1')
     })
 
     it('should default value', () => {
       let b = new B(1)
-      let res = _case(b).of([
-        [() => false, () => '() => false'],
-        [C, () => 'C']
-      ])
-      expect(res).to.equal(b)
+      let res = _case(b)
+        .of([
+          [() => false, () => '() => false'],
+          [C, () => 'C']
+        ])
+      expect(res)
+        .to.equal(b)
     })
   })
 })
